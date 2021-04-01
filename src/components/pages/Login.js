fdb7b3ea2a0a65933b898ce670fb8bf277f4c944
cody <codyvela0702@gmail.com>
2021-04-01 16:33:06 -0500

    added registration feature

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -6,22 +6,22 @@ export default function Login() {
   return (
     <div>
       <Container>
-        <h2 className="display-4 text-center pt-4">Login</h2>
+        <h2 className="display-4 text-center pt-4">Log In</h2>
         <Container className="d-flex justify-content-center pt-3">
-          <div className="border shadow-sm py-3 px-5">
+          <div className="border shadow-sm py-4 px-5">
             <Form>
               <Form.Group>
                 <Row>
                   <Form.Label>Username</Form.Label>
-                  <Form.Control size="sm" type="text"></Form.Control>
+                  <Form.Control size="lg" type="text"></Form.Control>
                 </Row>
                 <Row>
                   <Form.Label>Password</Form.Label>
-                  <Form.Control size="sm" type="text"></Form.Control>
+                  <Form.Control size="lg" type="password"></Form.Control>
                 </Row>
                 <Row className="pt-3">
                   <Button className="px-4" variant="primary" type="submit">
-                    Login
+                    Log In
                   </Button>
                 </Row>
               </Form.Group>
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,77 +1,165 @@
 import React, { useState } from "react";
 import "../../App.css";
-import { Container, Form, Col, Row, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Container, Form, Col, Row, Button, Alert } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import { useForm } from "react-hook-form";
+const axios = require("axios");
 
 export default function SignUp() {
-  const [data, setData] = useState({
-    firstName: "",
-    lastName: "",
-    userName: "",
-    email: "",
-    password: "",
-  });
+  //Used for the form | View docs -> https://react-hook-form.com/get-started
+  const { register, handleSubmit, errors } = useForm();
+  
+  //Used for setting the error message, could probably combine msg and msgVariant
+  const [msg, setMsg] = useState({ heading: "", body: "" });
+  const [msgVariant, setMsgVariant] = useState("");
+  const [showError, setShowError] = useState(false);
+  
+  //Used for redirecting, Successful Registration -> Login Page
+  const history = useHistory();
 
-  const handleChange = (e) => {
-    setData({ [e.target.name]: e.target.value });
-  };
+  const onSubmit = async (data, e) => {
+    //Verify the passwords match
+    if (data.password !== data.confirmPassword) {
+      //Render the error message
+      setMsgVariant("danger");
+      setMsg({ heading: "Error!", body: "Passwords do not match..." });
+      setShowError(true);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(data);
+      //Hide message after three seconds.
+      setTimeout(() => setShowError(false), 3000);
+      return;
+    } else {
+      //Drop the second password
+      delete data.confirmPassword;
+      try {
+        //Make a post request to the API to register the user
+        let res = await axios.post(
+          "http://localhost:8080/users/register",
+          data
+        );
+        //Display success message
+        setMsgVariant("success");
+        setMsg({ heading: "Success!", body: "Successfully Registered!" });
+        setShowError(true);
+
+        //Redirect the user to the login page after two seconds.
+        setTimeout(() => {
+          history.push("/login");
+        }, 2000);
+      } catch (error) {
+        if (error) {
+          //Handle the error message.
+          setMsgVariant("danger");
+          setMsg({ heading: "Error!", body: error.response.data });
+          setShowError(true);
+
+          //Hide the message after three seconds.
+          setTimeout(() => setShowError(false), 3000);
+          return;
+        }
+      }
+    }
   };
 
   return (
     <Container fluid className="pb-5">
       <h2 className="display-4 text-center pt-3">Sign Up</h2>
+      {/* Error message  */}
+      {showError ? (
+        <div className="d-flex justify-content-center">
+          <Alert className="py-3 px-5" variant={msgVariant}>
+            <Alert.Heading>{msg.heading}</Alert.Heading>
+            <p className="text-lead text-center">{msg.body}</p>
+          </Alert>
+        </div>
+      ) : (
+        <React.Fragment></React.Fragment>
+      )}
+      {/* Start of form */}
       <Container className="d-flex justify-content-center py-3">
-        <div className="border shadow-sm py-3 px-5">
-          <Form>
+        <div className="border shadow-sm py-4 px-5">
+          <Form onSubmit={handleSubmit(onSubmit)}>
             <Form.Group>
               <Row>
                 <Col>
                   <Form.Label>Firstname</Form.Label>
                   <Form.Control
                     name="firstName"
-                    size="sm"
+                    size="md"
+                    ref={register({ required: true })}
                     type="text"
-                    value={data.firstName}
-                    onChange={handleChange}
                   ></Form.Control>
+                  {errors.firstName && (
+                    <span className="text-danger">Firstname required!</span>
+                  )}
                 </Col>
                 <Col>
                   <Form.Label>Lastname</Form.Label>
                   <Form.Control
                     name="lastName"
-                    size="sm"
+                    size="md"
+                    ref={register({ required: true })}
                     type="text"
-                    value={data.lastName}
-                    onChange={handleChange}
                   ></Form.Control>
+                  {errors.lastName && (
+                    <span className="text-danger">Lastname required!</span>
+                  )}
                 </Col>
               </Row>
               <Row>
                 <Col>
                   <Form.Label>Username</Form.Label>
-                  <Form.Control size="sm" type="text"></Form.Control>
+                  <Form.Control
+                    size="md"
+                    ref={register({ required: true })}
+                    name="userName"
+                    type="text"
+                  ></Form.Control>
+                  {errors.userName && (
+                    <span className="text-danger">Username required!</span>
+                  )}
                 </Col>
               </Row>
               <Row>
                 <Col>
                   <Form.Label>Email</Form.Label>
-                  <Form.Control size="sm" type="email"></Form.Control>
+                  <Form.Control
+                    name="email"
+                    size="md"
+                    ref={register({ required: true })}
+                    type="email"
+                  ></Form.Control>
+                  {errors.email && (
+                    <span className="text-danger">Email required!</span>
+                  )}
                 </Col>
               </Row>
               <Row>
                 <Col>
                   <Form.Label>Password</Form.Label>
-                  <Form.Control size="sm" type="password"></Form.Control>
+                  <Form.Control
+                    name="password"
+                    size="md"
+                    ref={register({ required: true })}
+                    type="password"
+                  ></Form.Control>
+                  {errors.password && (
+                    <span className="text-danger">Passsword required!</span>
+                  )}
                 </Col>
               </Row>
               <Row>
                 <Col>
                   <Form.Label>Confirm Password</Form.Label>
-                  <Form.Control size="sm" type="password"></Form.Control>
+                  <Form.Control
+                    name="confirmPassword"
+                    size="md"
+                    ref={register({ required: true })}
+                    type="password"
+                  ></Form.Control>
+                  {errors.confirmPassword && (
+                    <span className="text-danger">Confirm Password!</span>
+                  )}
                 </Col>
               </Row>
               <Row className="pt-3">
@@ -86,7 +174,7 @@ export default function SignUp() {
         </div>
       </Container>
       <p className="text-center font-weight-light">
-        Already have an account? <Link to='/login'>Login here</Link>
+        Already have an account? <Link to="/login">Login here</Link>
       </p>
     </Container>
   );
