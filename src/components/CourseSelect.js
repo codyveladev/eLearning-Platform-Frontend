b06ab13330d9e947d3d5360e05a996a02489e7f8
Courtney Whitson <whitsoncourtney97@gmail.com>
2021-04-08 16:52:47 -0500

    Start to the Main Page

---
+++ src/components/CourseSelect.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import'../App.css';
+import './CourseSelect.css';
+
+function CourseSelect() {
+    return(
+      <div className='course-container'>
+        
+      </div>
+    );
+  }
+  
+  export default CourseSelect
